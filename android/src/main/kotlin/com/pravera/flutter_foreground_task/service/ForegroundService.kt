f98b0e21fdebda5b993e3d1ea330da365a9083b0
mohammad <developmenttest2022@gmail.com>
2023-08-01 18:46:30 +0300

    font convert to sp

--- android/src/main/kotlin/com/pravera/flutter_foreground_task/service/ForegroundService.kt
+++ android/src/main/kotlin/com/pravera/flutter_foreground_task/service/ForegroundService.kt
@@ -10,6 +10,7 @@ import android.graphics.*
 import android.net.wifi.WifiManager
 import android.os.*
 import android.util.Log
+import android.util.TypedValue
 import android.widget.RemoteViews
 import androidx.core.app.NotificationCompat
 import com.pravera.flutter_foreground_task.R
@@ -412,7 +413,7 @@ class ForegroundService : Service(), MethodChannel.MethodCallHandler {
             var subtitle = notificationOptions.todayNotificationData?.hijri?:""
             if (!notificationOptions.todayNotificationData?.gregorian.isNullOrEmpty())
                 subtitle += " - " + notificationOptions.todayNotificationData?.gregorian!!
-            val fontSize = 36f
+            val fontSize = 13f
             notificationLayout.setImageViewBitmap(
                 R.id.title_v2_1,
                 text2Bitmap3(
@@ -494,52 +495,6 @@ class ForegroundService : Service(), MethodChannel.MethodCallHandler {
                     fontSize
                 )
             )
-
-//            notificationLayout.setImageViewBitmap(
-//                R.id.title_1,
-//                text2Bitmap2(
-//                    notificationOptions.prayNotificationData?.titles?.get(0) ?: "",
-//                    notificationOptions.prayNotificationData?.times?.get(0) ?: "",
-//                    notificationOptions.todayNotificationData?.titleColor!!,
-//                    notificationOptions.todayNotificationData?.subtitleColor!!,
-//                )
-//            )
-//            notificationLayout.setImageViewBitmap(
-//                R.id.title_2,
-//                text2Bitmap2(
-//                    notificationOptions.prayNotificationData?.titles?.get(1) ?: "",
-//                    notificationOptions.prayNotificationData?.times?.get(1) ?: "",
-//                    notificationOptions.todayNotificationData?.titleColor!!,
-//                    notificationOptions.todayNotificationData?.subtitleColor!!,
-//                )
-//            )
-//            notificationLayout.setImageViewBitmap(
-//                R.id.title_3,
-//                text2Bitmap2(
-//                    notificationOptions.prayNotificationData?.titles?.get(2) ?: "",
-//                    notificationOptions.prayNotificationData?.times?.get(2) ?: "",
-//                    notificationOptions.todayNotificationData?.titleColor!!,
-//                    notificationOptions.todayNotificationData?.subtitleColor!!,
-//                )
-//            )
-//            notificationLayout.setImageViewBitmap(
-//                R.id.title_4,
-//                text2Bitmap2(
-//                    notificationOptions.prayNotificationData?.titles?.get(3) ?: "",
-//                    notificationOptions.prayNotificationData?.times?.get(3) ?: "",
-//                    notificationOptions.todayNotificationData?.titleColor!!,
-//                    notificationOptions.todayNotificationData?.subtitleColor!!,
-//                )
-//            )
-//            notificationLayout.setImageViewBitmap(
-//                R.id.title_5,
-//                text2Bitmap2(
-//                    notificationOptions.prayNotificationData?.titles?.get(4) ?: "",
-//                    notificationOptions.prayNotificationData?.times?.get(4) ?: "",
-//                    notificationOptions.todayNotificationData?.titleColor!!,
-//                    notificationOptions.todayNotificationData?.subtitleColor!!,
-//                )
-//            )
             val builder = Notification.Builder(this, notificationOptions.channelId + " Pray")
             builder.setOngoing(true)
             builder.setShowWhen(notificationOptions.showWhen)
@@ -902,7 +857,7 @@ class ForegroundService : Service(), MethodChannel.MethodCallHandler {
         paint.typeface = typeface
         paint.style = Paint.Style.FILL
         paint.color = titleColor
-        paint.textSize = 56f
+        paint.textSize = spToPx(18f)
         paint.textAlign = Paint.Align.RIGHT
 
         val paint2 = Paint()
@@ -911,7 +866,7 @@ class ForegroundService : Service(), MethodChannel.MethodCallHandler {
         paint2.typeface = typeface
         paint2.style = Paint.Style.FILL
         paint2.color = subtitleColor
-        paint2.textSize = 40f
+        paint2.textSize = spToPx(14f)
         paint2.textAlign = Paint.Align.RIGHT
         val rect = Rect()
         paint.getTextBounds(title, 0, title.length, rect)
@@ -964,7 +919,7 @@ class ForegroundService : Service(), MethodChannel.MethodCallHandler {
         paint.typeface = typeface
         paint.style = Paint.Style.FILL
         paint.color = titleColor
-        paint.textSize = fontSize
+        paint.textSize = spToPx(fontSize)
         paint.textAlign = Paint.Align.RIGHT
 
 
@@ -986,5 +941,12 @@ class ForegroundService : Service(), MethodChannel.MethodCallHandler {
         val canvas = Canvas(createBitmap)
         canvas.drawText(title, (width).toFloat(), (-paint.fontMetrics.ascent), paint)
         return createBitmap
+    }
+    private fun spToPx(sp: Float): Float {
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_SP,
+            sp,
+            resources.displayMetrics
+        )
     }
 }
